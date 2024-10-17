d90139f30f4c57664b37868b213c2de1cca25948
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-17 17:24:11 +0600

    Создал состояния: массив стран, код выбранной страны, объект с данными о выбранной стране. В первом хуке для загрузки списка стран: выполняю запрос к API ограниченный альфа кодом и именем. Во втором хуке для загрузки данных о выбранной стране: когда selectedCountryCode меняется - выполняю запрос по выбранной стране. В разметке вывожу компоненты

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Col, Container, Row } from 'react-bootstrap';
+import CountryList from './components/CountryList.tsx';
+import Country from './components/Country.tsx';
+import { CountryProps } from './types';
 
-const App = () => {
-    return (
-        <>
-
-        </>
-    )
+const App: React.FC = () => {
+  const [countries, setCountries] = useState<CountryProps[]>([]);
+  const [selectedCountryCode, setSelectedCountryCode] = useState<string | null>(null);
+  const [selectedCountry, setSelectedCountry] = useState<CountryProps | null>(null);
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const response = await axios.get('https://restcountries.com/v2/all?fields=alpha3Code,name');
+      setCountries(response.data);
+    };
+    fetchCountries();
+  }, []);
+  useEffect(() => {
+    if (selectedCountryCode) {
+      const fetchCountry = async () => {
+        const response = await axios.get(`https://restcountries.com/v2/alpha/${selectedCountryCode}`);
+        setSelectedCountry(response.data);
+      };
+      fetchCountry();
+    }
+  }, [selectedCountryCode]);
+  return (
+    <Container>
+      <Row>
+        <Col md={4}>
+          <CountryList
+            countries={countries}
+            onSelectCountry={setSelectedCountryCode}/>
+        </Col>
+        <Col md={8}>
+          <Country country={selectedCountry}/>
+        </Col>
+      </Row>
+    </Container>
+  )
 };
 
 export default App
